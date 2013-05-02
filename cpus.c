eec6e029107942aa26b006c6a3e5ab165f4cafef
Jan Kiszka <jan.kiszka@siemens.com>
2013-05-02 12:57:14 +0200

    realtime: Mark VCPU threads as PRIO_RT
    This will prioritize them one level below the iothread or other maximum-
    priority jobs if realtime is enabled.
    
    Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

--- cpus.c
+++ cpus.c
@@ -1021,7 +1021,7 @@ static void qemu_tcg_init_vcpu(CPUState *cpu)
         qemu_cond_init(cpu->halt_cond);
         tcg_halt_cond = cpu->halt_cond;
         qemu_thread_create(cpu->thread, qemu_tcg_cpu_thread_fn, cpu,
-                           QEMU_THREAD_JOINABLE);
+                           QEMU_THREAD_JOINABLE | QEMU_THREAD_PRIO_RT);
 #ifdef _WIN32
         cpu->hThread = qemu_thread_get_handle(cpu->thread);
 #endif
@@ -1041,7 +1041,7 @@ static void qemu_kvm_start_vcpu(CPUState *cpu)
     cpu->halt_cond = g_malloc0(sizeof(QemuCond));
     qemu_cond_init(cpu->halt_cond);
     qemu_thread_create(cpu->thread, qemu_kvm_cpu_thread_fn, cpu,
-                       QEMU_THREAD_JOINABLE);
+                       QEMU_THREAD_JOINABLE | QEMU_THREAD_PRIO_RT);
     while (!cpu->created) {
         qemu_cond_wait(&qemu_cpu_cond, &qemu_global_mutex);
     }
