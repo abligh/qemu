d88823ce299f4691016b357b2c596ecb8aff94bc
Alex Bligh <alex@alex.org.uk>
2013-07-25 22:33:12 +0100

    aio / timers: Convert aio_poll to use AioContext timers' deadline
    Convert aio_poll to use deadline based on AioContext's timers.
    
    aio_poll has been changed to return accurately whether progress
    has occurred. Prior to this commit, aio_poll always returned
    true if g_poll was entered, whether or not any progress was
    made. This required a change to tests/test-aio.c where an
    assert was backwards.

--- aio-win32.c
+++ aio-win32.c
@@ -98,6 +98,7 @@ bool aio_poll(AioContext *ctx, bool blocking)
     HANDLE events[MAXIMUM_WAIT_OBJECTS + 1];
     bool busy, progress;
     int count;
+    int timeout;
 
     progress = false;
 
@@ -110,6 +111,9 @@ bool aio_poll(AioContext *ctx, bool blocking)
         blocking = false;
         progress = true;
     }
+
+    /* Run timers */
+    progress |= timerlistgroup_run_timers(ctx->tlg);
 
     /*
      * Then dispatch any pending callbacks from the GSource.
@@ -174,8 +178,11 @@ bool aio_poll(AioContext *ctx, bool blocking)
 
     /* wait until next event */
     while (count > 0) {
-        int timeout = blocking ? INFINITE : 0;
-        int ret = WaitForMultipleObjects(count, events, FALSE, timeout);
+        int ret;
+
+        timeout = blocking ?
+            qemu_timeout_ns_to_ms(timerlistgroup_deadline_ns(ctx->tlg)) : 0;
+        ret = WaitForMultipleObjects(count, events, FALSE, timeout);
 
         /* if we have any signaled events, dispatch event */
         if ((DWORD) (ret - WAIT_OBJECT_0) >= count) {
@@ -214,6 +221,15 @@ bool aio_poll(AioContext *ctx, bool blocking)
         events[ret - WAIT_OBJECT_0] = events[--count];
     }
 
+    if (blocking) {
+        /* Run the timers a second time. We do this because otherwise aio_wait
+         * will not note progress - and will stop a drain early - if we have
+         * a timer that was not ready to run entering g_poll but is ready
+         * after g_poll. This will only do anything if a timer has expired.
+         */
+        progress |= timerlistgroup_run_timers(ctx->timer_list);
+    }
+
     assert(progress || busy);
-    return true;
+    return progress;
 }
